f5e4c6676dd03b38915bb8d3c38671b2f2c55ba5
Nicolas Miyasato <nmiyasato@gmail.com>
2023-10-10 01:04:42 -0300

    Renaming

--- Sources/Connector/Connector.swift
+++ Sources/Connector/Connector.swift
@@ -4,11 +4,11 @@ import Foundation
 public protocol Connector {
     associatedtype EndpointType: Endpoint
     var dataProvider: DataProvider { get }
-    func request(_ endpoint: EndpointType) async -> Result<EndpointType.Response, Error>
+    func fetch(from endpoint: EndpointType) async -> Result<EndpointType.Response, Error>
 }
 
 public extension Connector {
-    func request(_ endpoint: EndpointType) async -> Result<EndpointType.Response, Error> where EndpointType.Response: Decodable {
+    func fetch(from endpoint: EndpointType) async -> Result<EndpointType.Response, Error> where EndpointType.Response: Decodable {
         return await dataProvider.request(endpoint)
     }
 
