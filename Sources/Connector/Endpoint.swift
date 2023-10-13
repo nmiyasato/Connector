0a02ae3d8dd74d8b76bf1112069107f4eebcb545
Nicolas Miyasato <nmiyasato@gmail.com>
2023-10-13 15:31:18 -0300

    Updating Endpoint to calculate hostURL and path.

--- Sources/Connector/Endpoint.swift
+++ Sources/Connector/Endpoint.swift
@@ -3,11 +3,15 @@ import Foundation
 public protocol Endpoint {
     associatedtype Response: Decodable
     
-    var baseURL: URL { get }
-    var path: String? { get }
+    var endpointURL: URL { get }
     var httpMethod: HTTPMethod? { get }
     var headers: [String: String]? { get }
     var parameters: [String: Any]? { get }
+}
+
+extension Endpoint {
+    public var hostURL: URL { URL(string: endpointURL.scheme ?? "https:" + "//" + (endpointURL.host ?? ""))! }
+    public var path: String { endpointURL.pathComponents.joined(separator: "/").replacingOccurrences(of: "//", with: "/") }
 }
 
 public enum HTTPMethod: String {
