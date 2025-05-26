395c92d70d6c2ca6ff78db525f74d92f934be6b2
Nicolás Miyasato <nmiyasato@gmail.com>
2025-05-26 01:29:46 -0300

    Uses dynamic  directory

--- Plugins/RunMockolo/RunMockoloPlugin.swift
+++ Plugins/RunMockolo/RunMockoloPlugin.swift
@@ -3,14 +3,13 @@ import PackagePlugin
 @main struct RunMockoloPlugin: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         let generatedSourcePath = context.pluginWorkDirectory.appending("GeneratedMocks.swift")
-        let packageRoot = context.package.directory
 
         return [
             .prebuildCommand(
                 displayName: "Run mockolo",
                 executable: try context.tool(named: "mockolo").path,
                 arguments: [
-                    "-s", packageRoot.appending("Sources", "Connector").string,
+                    "-s", target.directory.string,
                     "-d", generatedSourcePath,
                 ],
                 outputFilesDirectory: context.pluginWorkDirectory
