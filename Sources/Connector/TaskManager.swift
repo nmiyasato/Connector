08b40d5c17774c896727d79056f77093881f142f
Nicolás Miyasato <nmiyasato@gmail.com>
2025-08-04 10:58:13 -0300

    Revert "Update Sources/Connector/TaskManager.swift"
    This reverts commit a6909aab3451785c83d041adf02da82f836f65ed.

--- Sources/Connector/TaskManager.swift
+++ Sources/Connector/TaskManager.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // A class-based wrapper for Task to enable identity comparison
-public final class TaskWrapper: Sendable {
+public class TaskWrapper: @unchecked Sendable {
     let task: Any
     let id: String
     
